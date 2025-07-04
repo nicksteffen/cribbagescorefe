90d4b296d3681b273fee088f71b81782a90f9acf
Nicholas Steffen <nicholassteffen@Nicholass-MacBook-Pro.local>
2025-07-04 10:34:12 -0400

    update site title and favicon, cleanup code

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
-// import Message from './components/Message.vue';
-
-      
-
-
-
-
-
 </script>
 
 <template>
